7088141273e0b9ee980a307cb20cedf457423ac1
Pranav Deshpande <deshpande.v.pranav@gmail.com>
2017-09-10 19:05:45 +0530

    More changes to movegen

--- move.cpp
+++ move.cpp
@@ -3,37 +3,81 @@
 
 // I will totally avoid the move generation for kings now
 // Let us worry about wrap arounds and out of boards later
+// First I will write code only for the light side
 
 void board::generateAllMoves() {
-  uint64_t temp, tempKing;
+  uint64_t tempLight, tempDark, tempKing, block;
   tempKing = king;
+  // Note that this is just a temporary vector for testing purposes
   std::vector <uint64_t> moveList;
+  block = light | dark;
+  
   if (side == LIGHT) {
     for(int i = 0; i < 64; i++) {
       if((1ULL << i) & light) {
-        
-        temp = light;
-        
-        // Remove the bit - 7
-        temp = temp & ~( 1ULL << i);
-        // Set the bit - 7
-        temp = temp | ( 1ULL << (i+7) );
-        // update moveList
-        moveList.push_back(temp);
-        
-        temp = light;
-        
-        // Remove the bit - 9
-        temp = temp & ~(1ULL << i);
-        // Set the bit - 9
-        temp = temp | ( 1ULL << (i+9) );
-        // update moveList
-        moveList.push_back(temp);
+
+      	// Normal moves
+
+        if(~(( 1ULL << (i+7) ) & block)) {
+	        tempLight = light;
+	        
+	        // Remove the bit - 7
+	        tempLight = tempLight & ~( 1ULL << i);
+	        // Set the bit - 7
+	        tempLight = tempLight | ( 1ULL << (i+7) );
+	        // update moveList
+	        moveList.push_back(tempLight);
+	    }   
+	    if(~(( 1ULL << (i+9) ) & block)) {   
+	        tempLight = light;
+	        
+	        // Remove the bit - 9
+	        tempLight = tempLight & ~(1ULL << i);
+	        // Set the bit - 9
+	        tempLight = tempLight | ( 1ULL << (i+9) );
+	        // update moveList
+	        moveList.push_back(tempLight);
+	    }
+
+	    // Captures
+
+        if(~(( 1ULL << (i+7) ) & block)) {
+        	if(( 1ULL << (i+14) ) & dark) {
+	        	tempLight = light;
+		        tempDark = dark;
+		        // Remove the bit from light
+		        tempLight = tempLight & ~( 1ULL << i);
+		        // Remove the bit 7 from dark
+		        tempDark = tempDark & ~( 1ULL << (i+7) );
+		        // Add light bit to position 14
+		        tempLight = tempLight | (1ULL << (i+14));
+		        // update moveList
+		        moveList.push_back(tempLight);
+		    }
+	    }   
+	    
+	    if(~(( 1ULL << (i+9) ) & block)) {
+        	if(( 1ULL << (i+18) ) & dark) {
+	        	tempLight = light;
+		        tempDark = dark;
+		        // Remove the bit from light
+		        tempLight = tempLight & ~( 1ULL << i);
+		        // Remove the bit 7 from dark
+		        tempDark = tempDark & ~( 1ULL << (i+9) );
+		        // Add light bit to position 14
+		        tempLight = tempLight | (1ULL << (i+18));
+		        // update moveList
+		        moveList.push_back(tempLight);
+		    }
+	    }   
       }
     }
   }
+  
   uint64_t ol = light, od = dark, ok = king;
+  
   std::cout << "The size of the moveList is: " << moveList.size() << std::endl;
+  
   for(auto it = moveList.begin(); it != moveList.end(); it++) {
     std::cout << std::endl;
     light = ol;
